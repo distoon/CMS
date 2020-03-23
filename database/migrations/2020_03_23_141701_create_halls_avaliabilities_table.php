3a992a34548aa085996473897a93c43b69cb78a9
Ahmed El-Behery <igl-@hotmail.com>
2020-03-23 16:34:20 +0200

    create tables migration

---
+++ database/migrations/2020_03_23_141701_create_halls_avaliabilities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHallsAvaliabilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('halls_avaliabilities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('course_id');
+            $table->time('time');
+            $table->binary('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('halls_avaliabilities');
+    }
+}
