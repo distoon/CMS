940264e73d63f0e9aedb29709cb39bbb49c1e4cc
Ali Mahmoud <aliy.mahmoud00@gmail.com>
2020-05-22 15:30:55 +0200

    unique id and username while adding student

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -40,8 +40,8 @@ class AdminController extends Controller
             'gpa' => $request->gpa,
             'department_id' => $request->department_id,
         ]);
-        $uniqueId = $student->created_at->format('Y');
-        $username = strtolower($request->firstName).strtolower($request->lastName).$uniqueId;
+        // $uniqueId = $student->created_at->format('Y');
+        // $username = strtolower($request->firstName).strtolower($request->lastName).$uniqueId;
         return redirect()->back();
     }
     public function getUpdateStudent($name){
